3a5ff50577f7e550accbf1b850f440f095c6d5b4
SethBRichards <25270821+SethBRichards@users.noreply.github.com>
2020-04-22 12:43:05 -0700

    Created keyboardControlledLimitedPlotter.py git file

---
+++ Python Files/Matplotlib & Control Testing/keyboardControlledLimitedPlotter.py
@@ -0,0 +1,81 @@
+from __future__ import print_function
+import math as m
+import matplotlib.pyplot as plt
+from inputs import get_key
+
+# Keyboard Controlled Limited Plotter
+#
+# This program allows the client to use arrow keys to
+# control the position of a circle on a plot over discrete time.
+#
+# The purpose of this project was to learn about how to code in python,
+# use matplotlib, take keyboard inputs, and work with discrete time control
+#
+# This code was create early on in my career, and thus is commented poorly
+
+switchBool = True
+x = 0
+
+
+def main():
+    global switchBool
+    global x
+    z =0
+    a = 0
+    c = 0
+    d = 0
+    e = 0
+
+    plt.axis([0, 100, 0, 50])
+    i = 0
+    # for i in range(50):
+    while i < 1000:
+        i = i + 1
+        events = get_key()
+        if events:
+            x = int(events[0].state)
+            switchBool = not switchBool
+            if switchBool:
+                d = x
+
+
+                if x == 72:
+                    a = 1
+                elif x == 80:
+                    a = -1
+                elif x == 77:
+                    a = 0
+
+
+                z = z + a
+
+
+
+
+                plt.plot(i/10, z, 'ro')
+
+
+
+                plt.plot(c/10, e, 'bo')
+                print(i / 10, z, "heyo")
+                print(c / 10, e, "oyeh")
+
+                c = i
+                e = z
+
+                print(i / 10, z, "      heyo")
+                print(c / 10, e, "      oyeh")
+
+                plt.pause(0.001)
+                print(i)
+                plt.copper()
+
+                plt.autoscale(enable=True, axis='both', tight=False)
+    #plt.autoscale(enable=True, axis='y', tight=False)
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
+
